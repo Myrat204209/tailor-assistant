43904c9e51484ab2de329e3cf5b139c9163018a0
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2024-11-07 15:08:03 +0500

    Commit #1

---
+++ android/app/src/main/kotlin/com/arassanusga/dap/foreman/assis/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.arassanusga.dap_foreman_assis
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:dap_foreman_assis/app/app.dart';
+import 'package:dap_foreman_assis/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}

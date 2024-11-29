d445c92d468f484ef56aeb511da81ee6f2b63344
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2024-11-29 17:38:10 +0500

    Commit #22

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -16,7 +16,8 @@ class HomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final pageController = usePageController();
-    final selectedIndex = useState(0);
+
+    final animatedButtonController = useMemoized(AnimatedButtonController.new);
 
     // Get theme-aware colors
     final colorScheme = Theme.of(context).colorScheme;
@@ -24,7 +25,7 @@ class HomePage extends HookWidget {
     final unSelectedColor = colorScheme.onPrimary;
 
     void onPageChange(int index) {
-      selectedIndex.value = index;
+      animatedButtonController.setIndex(index);
     }
 
     void onButtonTap(int index) {
@@ -33,7 +34,7 @@ class HomePage extends HookWidget {
         duration: const Duration(milliseconds: 100),
         curve: Curves.linear,
       );
-      selectedIndex.value = index;
+      animatedButtonController.setIndex(index);
     }
 
     return Stack(
@@ -45,32 +46,43 @@ class HomePage extends HookWidget {
           children: const [
             OrdersPage(),
             EmployeesPage(),
+            EmployeesPage(),
           ],
         ),
         Align(
           alignment: Alignment.bottomCenter,
           child: SizedBox(
-            width: 155,
+            width: 230,
             height: 80,
             child: AnimatedButtonBar(
-              controller: AnimatedButtonController(),
+              controller: animatedButtonController,
               radius: 50,
               backgroundColor: selectedColor,
               foregroundColor: unSelectedColor,
               children: [
                 _buildButton(
                   icon: Icons.dry_cleaning_rounded,
-                  isSelected: selectedIndex.value == 0,
+                  // isSelected: selectedIndex.value == 0,
+                  isSelected: animatedButtonController.index == 0,
                   selectedColor: selectedColor,
                   unSelectedColor: unSelectedColor,
                   onTap: () => onButtonTap(0),
                 ),
                 _buildButton(
                   icon: Icons.person_3_rounded,
-                  isSelected: selectedIndex.value == 1,
+                  // isSelected: selectedIndex.value == 1,
+                  isSelected: animatedButtonController.index == 1,
                   selectedColor: selectedColor,
                   unSelectedColor: unSelectedColor,
                   onTap: () => onButtonTap(1),
+                ),
+                _buildButton(
+                  icon: Icons.notes_rounded,
+                  // isSelected: selectedIndex.value == 1,
+                  isSelected: animatedButtonController.index == 2,
+                  selectedColor: selectedColor,
+                  unSelectedColor: unSelectedColor,
+                  onTap: () => onButtonTap(2),
                 ),
               ],
             ),
